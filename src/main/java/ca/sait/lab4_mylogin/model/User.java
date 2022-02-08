46267c73dd3cabd584bddba3127b2f832ee3e708
Meron Seyoum <meryato@gmail.com>
2022-02-08 13:21:22 -0700

    User class is create, getter and setter are also  setup

---
+++ src/main/java/ca/sait/lab4_mylogin/model/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Beans/Bean.java to edit this template
+ */
+package ca.sait.lab4_mylogin.model;
+
+import java.beans.*;
+import java.io.Serializable;
+
+/**
+ *
+ * @author merya
+ */
+public class User implements Serializable {
+    
+  private String username;
+private String password; 
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+
+    /**
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
